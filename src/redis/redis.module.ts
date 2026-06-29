c09f5652b1a59751c73f23f803f44f4844e0db5f
Milind Jamnekar <milindjamnekar502@gmail.com>
2026-06-29 23:39:46 +0530

    style: module change with style

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -11,7 +11,9 @@ export const REDIS_CLIENT = 'REDIS_CLIENT';
       provide: REDIS_CLIENT,
       inject: [ConfigService],
       useFactory: (config: ConfigService) =>
-        new IORedis(config.get<string>('REDIS_URL') ?? 'redis://localhost:6379'),
+        new IORedis(
+          config.get<string>('REDIS_URL') ?? 'redis://localhost:6379',
+        ),
     },
   ],
   exports: [REDIS_CLIENT],
